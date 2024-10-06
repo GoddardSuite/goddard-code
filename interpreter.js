927a7456b372ee135da987f42139685628e098cb
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2024-10-06 11:37:23 +0100

    Update interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -1,4 +1,4 @@
 function interpret(){
-  code=String(document.querySelector(".ace_text-input").innertext);
+  code=String(document.getElementsByClassName('question')[0].innerText);
   console.log(code);
 }
