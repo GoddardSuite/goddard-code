8715184194b6db0d68493ce48f60bced082703a0
cheese <108072844+deepfriedslipper@users.noreply.github.com>
2024-10-08 16:57:45 +0100

    began adding hotkeys

--- interpreter.js
+++ interpreter.js
@@ -1,55 +1,54 @@
-function interpret(){
+function interpret() {
   var code = String(editor.getValue()); //gets code
   throw code;
-  var lines=code.split("\n") //declares separate line
-  var lexer = function(line){//defines lexer function
-    var tokens=[];
+  var lines = code.split("\n") //declares separate line
+  var lexer = function (line) {//defines lexer function
+    var tokens = [];
     //checking each token (character) and classifying
     var isOperator = function (c) { return /[+\-*\/\^%=(),]/.test(c); },
-    digit = function (c) { return /[0-9]/.test(c); },
-    whiteSpace = function (c) { return /\s/.test(c); }, 
-    identifier = function (c) 
-      { return typeof c === "string" && !isOperator(c) && !isDigit(c) && !isWhiteSpace(c); };
+      digit = function (c) { return /[0-9]/.test(c); },
+      whiteSpace = function (c) { return /\s/.test(c); },
+      identifier = function (c) { return typeof c === "string" && !isOperator(c) && !isDigit(c) && !isWhiteSpace(c); };
     var tokens = [], c, i = 0;
-  var advance = function () { return c = input[++i]; };//defining commands for list of tokens
-  var addToken = function (type, value) {
-    tokens.push({
-      type: type,
-      value: value
-   });
-  };
-  while (i < input.length) {
-    c = input[i];
-    if (isWhiteSpace(c)) advance();//declares to ignore whitespace
-  else if (isOperator(c)) { //adding the operator token
-    addToken(c);
-    advance();
-  }
+    var advance = function () { return c = input[++i]; };//defining commands for list of tokens
+    var addToken = function (type, value) {
+      tokens.push({
+        type: type,
+        value: value
+      });
+    };
+    while (i < input.length) {
+      c = input[i];
+      if (isWhiteSpace(c)) advance();//declares to ignore whitespace
+      else if (isOperator(c)) { //adding the operator token
+        addToken(c);
+        advance();
+      }
 
-    else if (isDigit(c)) { //defines numbers as collections of digits
-  var num = c;
-  while (isDigit(advance())) num += c;
-  if (c === ".") {
-    do num += c; while (isDigit(advance()));
-  }
-  num = parseFloat(num);
-  if (!isFinite(num)) throw "Number is too large or too small for a 64-bit double.";
-  addToken("number", num);
-}
+      else if (isDigit(c)) { //defines numbers as collections of digits
+        var num = c;
+        while (isDigit(advance())) num += c;
+        if (c === ".") {
+          do num += c; while (isDigit(advance()));
+        }
+        num = parseFloat(num);
+        if (!isFinite(num)) throw "Number is too large or too small for a 64-bit double.";
+        addToken("number", num);
+      }
 
-    else if (isIdentifier(c)) { //every other character is identifier
-  var idn = c;
-  while (isIdentifier(advance())) idn += c;
-  addToken("identifier", idn);
-}
-    addToken("(end)")
+      else if (isIdentifier(c)) { //every other character is identifier
+        var idn = c;
+        while (isIdentifier(advance())) idn += c;
+        addToken("identifier", idn);
+      }
+      addToken("(end)")
 
-    return tokens;
-  }
-    for line in lines{
-      
+      return tokens;
     }
-    
+    for (var line in lines) {
+
+    }
+
     return tokens;
   };
 };
