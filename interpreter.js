fa964e6a90ec15e9697a4bcd3139bcd6bd05ba56
Karl-MrX <20StocksJ@roundwoodpark.co.uk>
2024-10-15 09:21:53 +0100

    Update interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -1,6 +1,7 @@
 function interpret() {
   var code = String(editor.getValue()); //gets code
-  var lines = code.split("\r"); //declares separate line
+  var lines = code.split("\n"); //declares separate line
+  window.alert(lines);
   var lexer = function (line) {//defines lexer function
     var tokens = [];
     //checking each token (character) and classifying
